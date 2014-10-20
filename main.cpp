8f55708df2c87fbf6d780530c375dd30fd57bb01
Lucas Åström <lucasas@kth.se>
2014-10-20 15:38:07 +0200

    moved sieve of eratosthenes to separate file

--- main.cpp
+++ main.cpp
@@ -5,38 +5,9 @@
 #include <cmath>
 using namespace std;
 
+#include "sieve_of_eratosthenes.hpp"
+
 typedef unsigned long long factor; //TODO Use GMP.
-
-vector<factor> sieve_of_eratosthenes(const factor& n)
-{
-  auto remaining = n;
-  vector<factor> factors;
-  vector<bool> is_prime (n, true);
-  
-  is_prime[0] = false;
-  is_prime[1] = false;
-  for (int i = 2; i <= sqrt(n); i++)
-  {
-    if (is_prime[i])
-    {
-      while (remaining % i == 0)
-      {
-        remaining /= i;
-        factors.push_back(i);
-      }
-
-      for (int j = i*i; j <= n; j += i)
-      {
-        is_prime[j] = false;
-      }
-    }
-  }
-
-  // Don't forget the last factor if one exists.
-  if (remaining > 1) factors.push_back(remaining);
-
-  return factors;
-}
 
 vector<factor> trial_division(const factor& n)
 {
@@ -62,7 +33,7 @@ clock_t start = clock();
 int main()
 { 
   factor n = 100;
-  auto factors = sieve_of_eratosthenes(n);
+  auto factors = sieve_of_eratosthenes<factor>(n);
   cout << "Composite number: " << n << endl;
   cout << "No. of factors found: " << factors.size() << endl;
   cout << "Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
