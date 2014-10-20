22cbeb8479b93a64dd456e8f2a80719e12a5a179
Carl Thomé <cthome@kth.se>
2014-10-20 16:46:07 +0200

    Updated main program to test all algorithms

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <functional>
+#include <utility>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
 #include <cmath>
-#include "sieve_of_eratosthenes.hpp"
 #include "trial_division.hpp"
+#include "sieve_of_eratosthenes.hpp"
+#include "sieve_of_atkin.hpp"
 using namespace std;
 
 typedef unsigned long long factor; //TODO Use GMP.
@@ -15,10 +18,27 @@ int main(int argc, char* argv[])
   if (argc != 2) cerr << "Missing argument." << endl;
   string arg1 = argv[1];
   factor n = strtol(arg1.c_str(), NULL, 10); // TODO Use GMP.
-  auto factors = sieve_of_eratosthenes<factor>(n);
-  cout << "Composite number: " << n << endl;
-  cout << "No. of factors found: " << factors.size() << endl;
-  cout << "Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-  cout << "Factors:" << endl;
-  for (auto f : factors) cout << f << endl;
+  
+  vector<pair<string, function<vector<factor>(factor)>>> factorization_algorithms = {
+    {"Trial Division", trial_division<factor>},
+    {"Sieve of Eratosthenes", sieve_of_eratosthenes<factor>},
+    {"Sieve of Atkin", sieve_of_atkin<factor>},
+  };
+
+  for (auto p : factorization_algorithms)
+  {
+    const auto& name = p.first;
+    const auto& factorization_algorithm = p.second;
+    cout << name << ':' << endl;
+
+    vector<factor> factors = factorization_algorithm(n);
+
+    cout << "  - Composite number: " << n << endl;
+    cout << "  - No. of factors found: " << factors.size() << endl;
+    cout << "  - Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
+    cout << "  - Factors:";
+    for (auto f : factors) cout << ' ' << f;
+    cout << endl << endl;
+  }
+  
 }
