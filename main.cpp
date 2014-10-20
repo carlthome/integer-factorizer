0e3c848154d944043ea430707a2c60543c7be043
Carl Thomé <cthome@kth.se>
2014-10-20 18:35:13 +0200

    Launch program with specific factorization algorithm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <functional>
 #include <utility>
 #include <cstdlib>
@@ -10,34 +11,50 @@
 #include "sieve_of_eratosthenes.hpp"
 #include "sieve_of_atkin.hpp"
 using namespace std;
+typedef mpz_class factor;
+map<string, function<vector<factor>(factor)>> factorization_algorithms = {
+  {"Trial Division", trial_division},
+  {"Sieve of Eratosthenes", sieve_of_eratosthenes},
+  {"Sieve of Atkin", sieve_of_atkin},
+};
 
 clock_t start = clock();
 int main(int argc, char* argv[])
 {
-  if (argc != 2) cerr << "Missing argument." << endl;
-  string arg1 = argv[1];
-  mpz_class n(arg1.c_str()); // TODO Use GMP.
-  
-  vector<pair<string, function<vector<mpz_class>(mpz_class)>>> factorization_algorithms = {
-    {"Trial Division", trial_division},
-    {"Sieve of Eratosthenes", sieve_of_eratosthenes},
-    {"Sieve of Atkin", sieve_of_atkin},
-  };
-
-  for (auto p : factorization_algorithms)
+  if (argc < 2)
   {
-    const auto& name = p.first;
-    const auto& factorization_algorithm = p.second;
-    cout << name << ':' << endl;
-
-    vector<mpz_class> factors = factorization_algorithm(n);
-
-    cout << "  - Composite number: " << n << endl;
-    cout << "  - No. of factors found: " << factors.size() << endl;
-    cout << "  - Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-    cout << "  - Factors:";
-    for (auto f : factors) cout << ' ' << f;
-    cout << endl << endl;
+    cerr << "Missing argument." << endl;
+    return -1;
   }
   
+  string arg1 = argv[1];
+  const factor n = strtol(arg1.c_str(), NULL, 10);
+  
+  auto go = [] (factor n, string name, function<vector<factor>(factor)> factorization_algorithm) {
+    vector<factor> factors = factorization_algorithm(n);
+    cout << name << ':' << endl;
+    cout << "  - Composite number: " << n << endl;
+    cout << "  - No. of factors found: " << factors.size() << endl;
+    cout << "  - Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << " seconds" << endl;
+    cout << "  - Factors:";
+    for (auto f : factors) cout << ", " << f;
+    cout << endl << endl;
+  };
+
+  if (argc > 2)
+  {
+    for (int i = 2; i < argc; ++i)
+    {
+      string s = argv[i];
+      auto f = factorization_algorithms[s];
+      go(n, s, f);
+    }
+  }
+  else
+  {
+    if (argc == 2) for (auto p : factorization_algorithms)
+    {
+      go(n, p.first, p.second);
+    }
+  }
 }
