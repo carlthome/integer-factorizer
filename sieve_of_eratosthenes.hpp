358daff73488b8c6cf570a2eeacfcaab7468e568
Lucas Åström <lucasas@kth.se>
2014-10-21 02:22:36 +0200

    added bit limit to eratosthenes, it also uses the square root instead of the bit limit if that's lower

--- sieve_of_eratosthenes.hpp
+++ sieve_of_eratosthenes.hpp
@@ -1,17 +1,29 @@
 #pragma once
 #include <vector>
 #include <climits>
+#include <iostream>
 using namespace std;
 
 inline vector<mpz_class> sieve_of_eratosthenes(const mpz_class& n)
 {
-  auto remaining = n;
+  mpz_class remaining = n;
   vector<mpz_class> factors;
-  vector<bool> is_prime (n.get_ui(), true);
+
+  unsigned int limit = pow(2, 26);
+  mpz_class tmp;
+  mpz_root(tmp.get_mpz_t(), n.get_mpz_t(), 2);
+  tmp++;
+  if (tmp.fits_uint_p() && tmp < limit)
+  {
+    limit = tmp.get_ui();
+    cout << "limit now: " << limit << endl;
+  }
+
+  vector<bool> is_prime (limit, true);
   
   is_prime[0] = false;
   is_prime[1] = false;
-  for (int i = 2; i <= sqrt(n); i++)
+  for (int i = 2; i < limit; i++)
   {
     if (is_prime[i])
     {
@@ -21,7 +33,7 @@ inline vector<mpz_class> sieve_of_eratosthenes(const mpz_class& n)
         factors.push_back(i);
       }
 
-      for (int j = i*i; j <= n; j += i)
+      for (int j = i*i; j < limit; j += i)
       {
         is_prime[j] = false;
       }
