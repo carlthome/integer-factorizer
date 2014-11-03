2d89fad76d0e1c2108ad7767f8accae835d4b00a
Lucas Åström <lucasas@kth.se>
2014-11-03 14:48:50 +0100

    added a random mpz int function

--- utils.hpp
+++ utils.hpp
@@ -1,7 +1,19 @@
 #pragma once
 #include <vector>
 #include <cmath>
+#include <time.h>
 using namespace std;
+
+namespace
+{
+  // random int from min to max inclusive
+  mpz_class rand_range(const mpz_class& min, const mpz_class& max)
+  {
+    static gmp_randclass r (gmp_randinit_default);
+    r.seed(time(NULL));
+    return min + (r.get_z_range(max - min + 1));
+  }
+}
 
 inline mpz_class gcd_iter(const mpz_class& a, const mpz_class& b)
 {
