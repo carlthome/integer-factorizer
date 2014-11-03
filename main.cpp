f2c327a40541b00b0a3ed74da8434944d800e7e0
Carl Thomé <cthome@kth.se>
2014-11-03 12:14:48 +0100

    Added interactive mode (with a goto because Lucas loves those!)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
-#include <gmpxx.h>
+#include <mpirxx.h>
 #include "trial_division.hpp"
 #include "sieve_of_eratosthenes.hpp"
 #include "sieve_of_atkin.hpp"
@@ -15,32 +15,39 @@
 using namespace std;
 typedef mpz_class factor;
 map<string, function<vector<factor>(factor)>> factorization_algorithms = {
-  {"Trial Division", trial_division},
-  {"Sieve of Eratosthenes", sieve_of_eratosthenes},
-  {"Sieve of Atkin", sieve_of_atkin},
-  {"Fermat Factorization", fermat_factorization},
+  { "Trial Division", trial_division },
+  { "Sieve of Eratosthenes", sieve_of_eratosthenes },
+  { "Sieve of Atkin", sieve_of_atkin },
+  { "Fermat Factorization", fermat_factorization },
 };
 
 clock_t start = clock();
 int main(int argc, char* argv[])
 {
+start:
+  string arg1;
   if (argc < 2)
   {
-    cerr << "Missing argument." << endl;
-    return -1;
+    cout << "Enter number: ";
+    cin >> arg1;
+    cout << endl << "Entered " << arg1 << endl;
   }
-  
-  string arg1 = argv[1];
-  const factor n = mpz_class(arg1.c_str());
-  
-  auto go = [] (factor n, string name, function<vector<factor>(factor)> factorization_algorithm) {
+  else
+  {
+    arg1 = argv[1];
+  }
+
+  const factor n = mpz_class(arg1, 10);
+
+  auto go = [](factor n, string name, function<vector<factor>(factor)> factorization_algorithm)
+  {
     vector<factor> factors = factorization_algorithm(n);
     cout << name << ':' << endl;
     cout << "  - Composite number: " << n << endl;
     cout << "  - No. of factors found: " << factors.size() << endl;
     cout << "  - Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << " seconds" << endl;
     cout << "  - Factors:";
-    for (auto f : factors) cout << ", " << f;
+    for (auto f : factors) cout << f << ", ";
     cout << endl << endl;
   };
 
@@ -55,9 +62,15 @@ int main(int argc, char* argv[])
   }
   else
   {
-    if (argc == 2) for (auto p : factorization_algorithms)
+    for (auto p : factorization_algorithms)
     {
       go(n, p.first, p.second);
     }
   }
+
+  string answer;
+  cout << "Done?" << endl;
+  cin >> answer;
+  if (answer == "y") return 0;
+  else goto start;
 }
