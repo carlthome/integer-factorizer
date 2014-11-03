a3f733b26c7922270beaa5608519b3f91a76c6eb
Lucas Åström <lucasas@kth.se>
2014-11-03 14:49:31 +0100

    added milles rabit probabilistic primality test

--- utils.hpp
+++ utils.hpp
@@ -31,3 +31,60 @@ inline mpz_class gcd_iter(const mpz_class& a, const mpz_class& b)
 
   return abs(u);
 }
+
+// return true if it PROBABLY is prime
+inline bool miller_rabin_prime(const mpz_class & n, unsigned int k)
+{
+  if (mpz_odd_p(n.get_mpz_t()) == 0)
+  {
+    return false;
+  }
+
+  mpz_class m, d;
+  d = m = n - 1;
+  mpz_class zero = mpz_class(0);
+  mpz_class one = mpz_class(1);
+  unsigned int s = 0;
+
+  while (mpz_even_p(d.get_mpz_t()))
+  {
+    s++;
+    d /= 2;
+  }
+
+  for (unsigned int i = 0; i < k; i++)
+  {
+    mpz_class a = rand_range(2, n - 2);
+    mpz_class x;
+    // x = a^d mod n
+    mpz_powm(x.get_mpz_t(), a.get_mpz_t(), d.get_mpz_t(), n.get_mpz_t());
+
+    if (x == one || x == m)
+    {
+      continue;
+    }
+
+    bool maybe_prime = false;
+    for (unsigned int j = 0; j < s - 1; j++)
+    {
+      mpz_powm_ui(x.get_mpz_t(), x.get_mpz_t(), 2, n.get_mpz_t());
+      if (x == one)
+      {
+        return false;
+      }
+      if (x == m)
+      {
+        maybe_prime = true;
+        break;
+      }
+    }
+
+    if (maybe_prime)
+    {
+      continue;
+    }
+    return false;
+  }
+
+  return true;
+}
