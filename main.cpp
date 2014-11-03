0d0f45228f1e5770393e8cc311311a7181cb5eda
Lucas Åström <lucasas@kth.se>
2014-11-03 15:14:07 +0100

    changed main to completely factorize using pollard rho and output intermediate results

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <map>
 #include <functional>
 #include <utility>
@@ -13,64 +14,33 @@
 #include "pollard_rho.hpp"
 #include "utils.hpp"
 using namespace std;
-typedef mpz_class factor;
-map<string, function<vector<factor>(factor)>> factorization_algorithms = {
-  {"Trial Division", trial_division},
-  {"Sieve of Eratosthenes", sieve_of_eratosthenes},
-  {"Fermat Factorization", fermat_factorization},
-  {"Pollard Rho", pollard_rho},
-};
 
 clock_t start = clock();
 int main(int argc, char* argv[])
 {
-start:
-  string arg1;
-  if (argc < 2)
-  {
-    cout << "Enter number: ";
-    cin >> arg1;
-    cout << endl << "Entered " << arg1 << endl;
-  }
-  else
-  {
-    arg1 = argv[1];
-  }
+  queue<mpz_class> todo;
+  cout << "Factoring number: " << argv[1] << endl;
+  todo.push(mpz_class(argv[1]));
 
-  const factor n = mpz_class(arg1, 10);
-
-  auto go = [](factor n, string name, function<vector<factor>(factor)> factorization_algorithm)
+  while (!todo.empty())
   {
-    vector<factor> factors = factorization_algorithm(n);
-    cout << name << ':' << endl;
-    cout << "  - Composite number: " << n << endl;
-    cout << "  - No. of factors found: " << factors.size() << endl;
-    cout << "  - Runtime: " << (double)(clock() - start) / CLOCKS_PER_SEC << " seconds" << endl;
-    cout << "  - Factors: ";
-    for (auto f : factors) cout << f << ", ";
-    cout << endl << endl;
-  };
+    auto& next = todo.front();
+    auto factors = pollard_rho(next);
 
-  if (argc > 2)
-  {
-    for (int i = 2; i < argc; ++i)
+    for (auto& fac : factors)
     {
-      string s = argv[i];
-      auto f = factorization_algorithms[s];
-      go(n, s, f);
+      if (miller_rabin_prime(fac, 50))
+      {
+        cout << fac << endl;
+      }
+      else
+      {
+        todo.push(fac);
+      }
     }
-  }
-  else
-  {
-    for (auto p : factorization_algorithms)
-    {
-      go(n, p.first, p.second);
-    }
+
+    todo.pop();
   }
 
-  string answer;
-  cout << "Done?" << endl;
-  cin >> answer;
-  if (answer == "y") return 0;
-  else goto start;
+  cout << "Done!" << endl;
 }
