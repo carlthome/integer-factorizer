3f109f290480ca920ca761cefc4c3ef36aebc47b
Lucas Åström <lucasas@kth.se>
2014-11-15 20:14:50 +0100

    quadratic sieving part is done

--- quadratic_sieve.hpp
+++ quadratic_sieve.hpp
@@ -8,6 +8,8 @@ using namespace std;
 
 namespace
 {
+  const unsigned int CHUNK_SIZE = 65536;
+
   unsigned int optimal_B(const mpz_class& n)
   {
     // reference: http://www.andrew-g-west.com/docs/thesis_slides.pdf
@@ -77,6 +79,85 @@ inline vector<mpz_class> quadratic_sieve(const mpz_class& n)
     tmp = i1 - sqrt_n;
     tmp = ((tmp % factor_base[p]) + factor_base[p]) % factor_base[p];
     factor_base_indices[1][p] = tmp.get_ui();
+  }
+
+  auto Y = vector<float>(M * 2);
+
+  auto X = vector<unsigned int>();
+  auto smooth = vector<vector<unsigned int>>();
+  unsigned int start = 0;
+  unsigned int end = CHUNK_SIZE;
+  // we want at least 20 more smooth numbers than "needed",
+  // to make sure that we get a dependancy that factorizes n
+  while (smooth.size() < (factor_base.size() + 20))
+  {
+    for (unsigned int t = 1; t < CHUNK_SIZE; t++)
+    {
+      // y = (sqrt(n) + x)^2 - n
+      mpz_class y = (sqrt_n + t + start) * (sqrt_n + t + start) - n;
+
+      // we estimate the 2 logarithm by counting the number of bits
+      Y[t] = mpz_sizeinbase(y.get_mpz_t(), 2);
+    }
+
+    // time to sieve!
+    for (unsigned int p = 0; p < factor_base.size(); p++)
+    {
+      float lg = log(factor_base[p]) / log(2);
+
+      for (unsigned int t = 0; t < 2; t++)
+      {
+        while (factor_base_indices[t][p] < end)
+        {
+          Y[factor_base_indices[t][p] - start] -= lg;
+          factor_base_indices[t][p] += factor_base[p];
+        }
+
+        // if p = 2 we only have one modular root
+        if (factor_base[p] == 2)
+        {
+          break;
+        }
+      }
+    }
+
+    float threshold = log(factor_base.back()) / log(2);
+
+    for (unsigned int i = 0; i < CHUNK_SIZE; i++)
+    {
+      if (abs(Y[i]) < threshold)
+      {
+        mpz_class y = (sqrt_n + i + start) * (sqrt_n + i + start) - n;
+        smooth.emplace_back();
+
+        for (unsigned int p = 0; p < factor_base.size(); p++)
+        {
+          while (mpz_divisible_ui_p(y.get_mpz_t(), factor_base[p]))
+          {
+            mpz_divexact_ui(y.get_mpz_t(), y.get_mpz_t(), factor_base[p]);
+            smooth.back().push_back(p);
+          }
+        }
+
+        if (y == 1)
+        {
+          // smooth! woop woop
+
+          X.push_back(i + start);
+          if (smooth.size() >= (factor_base.size() + 20))
+          {
+            break;
+          }
+        }
+        else
+        {
+          smooth.pop_back();
+        }
+      }
+    }
+
+    start += CHUNK_SIZE;
+    end += CHUNK_SIZE;
   }
 
   return factors;
