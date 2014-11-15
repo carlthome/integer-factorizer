c064b0a4ecd08de8e3f99ae57a5784c71eb6d801
Lucas Åström <lucasas@kth.se>
2014-11-15 16:49:25 +0100

    quadratic sieve now finds indices for sieving

--- quadratic_sieve.hpp
+++ quadratic_sieve.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <vector>
+#include <utility>
 #include "utils.hpp"
-#include "powm.hpp"
+#include "tonelli_shanks.hpp"
 #include <iostream>
 using namespace std;
 
@@ -34,29 +35,49 @@ namespace
 
 inline vector<mpz_class> quadratic_sieve(const mpz_class& n)
 {
-  vector<mpz_class> factors, factor_base;
+  auto factors = vector<mpz_class>();
   mpz_class a, tmp;
   mpz_sqrt(a.get_mpz_t(), n.get_mpz_t());
   // round up instead of down
   a++;
   // ~magic number~, needs to be different for different n
   auto B = optimal_B(n);
+  // sieving range
   auto M = B * B * B;
-  // number of primes below B, rough estimate
+
   auto primes = sieve_of_eratosthenes(B);
 
   // TODO: try this when it works
   //primes.insert(primes.begin(), -1);
 
+  auto factor_base = vector<unsigned int>();
   for (unsigned int i = 0; i < primes.size(); i++)
   {
     mpz_class p = primes[i];
     if (mpz_legendre(n.get_mpz_t(), p.get_mpz_t()) == 1)
     {
-      factor_base.push_back(p);
+      factor_base.push_back(primes[i]);
     }
   }
 
+  auto factor_base_indices = vector<vector<unsigned int>>(2, vector<unsigned int>(factor_base.size()));
+  auto sqrt_n = sqrt(n);
+
+  // find sieving indices for each factor base number
+  for (unsigned int p = 0; p < factor_base.size(); p++)
+  {
+    mpz_class tmp = n % mpz_class(factor_base[p]);
+    unsigned int i0, i1;
+    tie(i0, i1) = tonelli_shanks(tmp.get_ui(), factor_base[p]);
+
+    tmp = i0 - sqrt_n;
+    tmp = ((tmp % factor_base[p]) + factor_base[p]) % factor_base[p];
+    factor_base_indices[0][p] = tmp.get_ui();
+
+    tmp = i1 - sqrt_n;
+    tmp = ((tmp % factor_base[p]) + factor_base[p]) % factor_base[p];
+    factor_base_indices[1][p] = tmp.get_ui();
+  }
 
   return factors;
 }
